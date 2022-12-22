3ed7c3ada9a03c2fb07ffb9e15818599e095562b
Collin <collin-chua@hotmail.com>
2022-12-22 14:38:06 +0800

    Redid and refactor the logic

--- range.js
+++ range.js
@@ -7,12 +7,24 @@ const range = (start, end, iteration = 1) => {
 }
 */
 
+/*
 const range = (start, end, iteration = 1) => {
     let arr = [];
     if (end > start) {
         for (let i = start; i < end; i += iteration) arr.push(i);
     } else {
         for (let i = start; i > end; iteration > 0 ? i -= iteration: i += iteration) arr.push(i);
+    }
+    return arr
+};
+*/
+
+let range = (start, end, iteration = 1) => {
+    let arr = [];
+    if (end < start) {
+        for (let i = start; i > end; iteration > 0 ? i -= iteration: i += iteration) arr.push(i);
+    } else {
+        for (let i = start; i < end; i += iteration) arr.push(i);
     }
     return arr
 };
