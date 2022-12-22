c947c0cf7c1c604ab3573c0a053c170470f14410
Collin <collin-chua@hotmail.com>
2022-12-22 12:27:42 +0800

    Added my first file: JavaScript range function from Python

---
+++ range.js
@@ -0,0 +1,5 @@
+// Adding range() function from Python to JavaScript
+const range = (start, end, iteration = 1) => {
+    let arr = [];
+    for (let i = start; i < end; i += iteration) arr.push(i);
+}
