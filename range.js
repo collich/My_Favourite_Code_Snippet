07ccd1e70b77622530f1bd81ca4fddd655436583
Collin <collin-chua@hotmail.com>
2022-12-22 14:30:32 +0800

    Added support for negative list

--- range.js
+++ range.js
@@ -1,6 +1,18 @@
 // Adding range() function from Python to JavaScript
+/*
 const range = (start, end, iteration = 1) => {
     let arr = [];
     for (let i = start; i < end; i += iteration) arr.push(i);
     return arr;
 }
+*/
+
+const range = (start, end, iteration = 1) => {
+    let arr = [];
+    if (end > start) {
+        for (let i = start; i < end; i += iteration) arr.push(i);
+    } else {
+        for (let i = start; i > end; iteration > 0 ? i -= iteration: i += iteration) arr.push(i);
+    }
+    return arr
+};
